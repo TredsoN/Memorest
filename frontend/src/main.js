8e25c8ca4693a39f7f8421334b79b9ef3ba38ddb
cycychenyi <chenyi111@whu.edu.cn>
2020-04-14 22:00:16 +0800

    refactor: 调整 Router 和 Apollo 的结构

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,14 @@
 import Vue from 'vue'
 import router from './router/router.js'
-import Router from 'vue-router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import ApolloClient from 'apollo-boost'
-import VueApollo from 'vue-apollo'
+import apolloProvider from './graphql/apollo'
 import App from './App.vue'
 
-Vue.use(VueApollo);
-Vue.use(Router);
+
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
-const apolloClient = new ApolloClient({
-    uri: 'http://localhost:3000/graphql'
-});
-
-const apolloProvider = new VueApollo({
-    defaultClient: apolloClient,
-});
 
 new Vue({
     router,
