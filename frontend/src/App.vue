9c71df43cc141c082268b1fae3cfb2dc622bb1c9
TredsoN <32932125+TredsoN@users.noreply.github.com>
2020-04-14 13:18:04 +0800

    添加reload方法
    调用此方法可以刷新当前界面

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view v-if="isRouterAlive"></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-  components: {
+  provide() {
+    return {
+      reload: this.reload
+    }
+  },
+  data() {
+    return{
+      isRouterAlive: true
+    }
+  },
+  methods: {
+    reload() {
+      this.isRouterAlive = false
+      this.$nextTick(function () {
+        this.isRouterAlive = true
+      })
+    }
   }
 }
 </script>
