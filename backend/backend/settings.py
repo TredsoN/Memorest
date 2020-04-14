f4e7b90456f43dd1e2cd0a245e9c2100feb931af
ZhangGe <fforkboat@outlook.com>
2020-04-14 14:43:24 +0800

    chore: 添加部署时的静态文件地址

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/memorest/static/'
 
 # Auth
 AUTH_USER_MODEL = 'user.User'
