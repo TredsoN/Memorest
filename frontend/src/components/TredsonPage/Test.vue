f9cd75e03cf1cfaed240c11face5e0f5206c4563
TredsoN <32932125+TredsoN@users.noreply.github.com>
2020-04-14 12:16:56 +0800

    安装vue-router

---
+++ frontend/src/components/TredsonPage/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="test">
+        <input v-model="message"/>
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Test",
+    data(){
+        return{
+            message: ""
+        }
+    }
+}
+</script>
