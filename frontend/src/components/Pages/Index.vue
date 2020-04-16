8ec4ced378c366a2851dd82d503ac55b788e480e
TredsoN <32932125+TredsoN@users.noreply.github.com>
2020-04-16 18:10:26 +0800

    Update Index.vue

--- frontend/src/components/Pages/Index.vue
+++ frontend/src/components/Pages/Index.vue
@@ -5,11 +5,9 @@
         <router-link :to="{ name: 'login' }">
             <el-button v-if="!isLogined">登录</el-button>
         </router-link>
+        
         <router-link :to="{ name: 'personal' }">
             <el-button v-if="isLogined">个人中心</el-button>
-        </router-link>
-         <router-link :to="{ name: 'passwordfind' }">
-            <el-button>找回密码</el-button>
         </router-link>
     </div>
 </template>
