2f91a80ec4466891c8edaaa8ae3c3b6c50622c98
w1009296924 <52151272+w1009296924@users.noreply.github.com>
2020-04-18 20:17:58 +0800

    Add files via upload

--- backend/backend/schema.py
+++ backend/backend/schema.py
@@ -3,15 +3,15 @@ import graphene
 from graphql_auth.schema import UserQuery, MeQuery
 
 from user.schema import AuthMutation
+from memory.schema import MemoryMutation, MemoryQuery
 
 
-class Query(UserQuery, MeQuery, graphene.ObjectType):
+class Query(UserQuery, MeQuery, MemoryQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(AuthMutation, graphene.ObjectType):
+class Mutation(AuthMutation, MemoryMutation, graphene.ObjectType):
     pass
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
-
