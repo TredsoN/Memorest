fe2e2c45516c754e9f5584a93d330a444be1a587
w1009296924 <52151272+w1009296924@users.noreply.github.com>
2020-05-10 17:13:13 +0800

    Add files via upload

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -20,22 +20,6 @@ app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
-
-# 设置定时任务
-# app.conf.update(
-#     CELERYBEAT_SCHEDULE={
-#         'memoryLoss': {  # 名字随意起
-#             'task': 'memory.tasks.memoryLoss',  # 指定定时任务路径
-#             'schedule': timedelta(seconds=20),  # 每20执行一次
-#         },
-#         # 'send_email': {
-#         #     'task': 'app01.tasks.email',
-#         #     'schedule': crontab(hour='18',minute='45'),  # 每天18点45分执行
-#         # }
-#     }
-# )
-
-
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
